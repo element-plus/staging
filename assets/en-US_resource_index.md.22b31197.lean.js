cbbb042ee3e932697f7eff226b9611ec9969f6ae
ElementPlusBot <hello@element-plus.org>
2022-03-24 12:58:37 +0000

    Deploying to main from @ element-plus/element-plus@f2d23ba0491d6ee68b31495d84cf645992f9ae9a 🚀

---
+++ assets/en-US_resource_index.md.22b31197.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as a,a as o,r,o as n}from"./app.d4d00553.js";const f='{"title":"","description":"","frontmatter":{"page":true,"lang":"en-US"},"headers":[],"relativePath":"en-US/resource/index.md","lastUpdated":1648126458000}',s={};function c(p,d,_,i,l,u){const e=r("Resource");return n(),a("div",null,[o(e)])}var x=t(s,[["render",c]]);export{f as __pageData,x as default};
