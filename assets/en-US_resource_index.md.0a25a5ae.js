ddbe17f7ee899ad3e389a57315ae96393fe4685c
ElementPlusBot <hello@element-plus.org>
2022-04-05 03:01:14 +0000

    Deploying to main from @ element-plus/element-plus@a31fc8a460626fde077b9b52bcf36ece233fba4f 🚀

---
+++ assets/en-US_resource_index.md.0a25a5ae.js
@@ -0,0 +1,1 @@
+import{_ as t,c as a,a as o,r,o as n}from"./app.f1c4a678.js";const f='{"title":"","description":"","frontmatter":{"page":true,"lang":"en-US"},"headers":[],"relativePath":"en-US/resource/index.md","lastUpdated":1649127348000}',s={};function c(p,d,_,i,l,u){const e=r("Resource");return n(),a("div",null,[o(e)])}var x=t(s,[["render",c]]);export{f as __pageData,x as default};
