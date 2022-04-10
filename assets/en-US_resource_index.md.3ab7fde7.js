2ecbd3fb1951d3cfbaf666c845bdb6b569a23389
ElementPlusBot <hello@element-plus.org>
2022-04-10 09:35:13 +0000

    Deploying to main from @ element-plus/element-plus@5849909577caf7732369d451ff36e8c7910cff50 🚀

--- assets/en-US_resource_index.md.3ab7fde7.js
+++
@@ -1,1 +0,0 @@
-import{c as t,a,r as o,o as r}from"./app.c9f44abc.js";const p='{"title":"","description":"","frontmatter":{"page":true,"lang":"en-US"},"headers":[],"relativePath":"en-US/resource/index.md","lastUpdated":1649573281000}',n={},d=Object.assign(n,{setup(s){return(c,_)=>{const e=o("Resource");return r(),t("div",null,[a(e)])}}});export{p as __pageData,d as default};
---
+++ assets/en-US_resource_index.md.63e8c68c.lean.js
@@ -0,0 +1,1 @@
+import{c as t,a,r as o,o as r}from"./app.38ab0787.js";const p='{"title":"","description":"","frontmatter":{"page":true,"lang":"en-US"},"headers":[],"relativePath":"en-US/resource/index.md","lastUpdated":1649583008000}',n={},d=Object.assign(n,{setup(s){return(c,_)=>{const e=o("Resource");return r(),t("div",null,[a(e)])}}});export{p as __pageData,d as default};
