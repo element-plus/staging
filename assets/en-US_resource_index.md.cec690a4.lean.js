290ef1152fddbf685f459e5d2b33ef1e50084106
ElementPlusBot <hello@element-plus.org>
2022-04-20 09:06:40 +0000

    Deploying to main from @ element-plus/element-plus@de29873f4029c665a970649ebcd405dcd1fb78ad 🚀

--- assets/en-US_resource_index.md.cec690a4.lean.js
+++
@@ -1,1 +0,0 @@
-import{c as t,a,r as o,o as r}from"./app.5e1a9c9c.js";const p='{"title":"","description":"","frontmatter":{"page":true,"lang":"en-US"},"headers":[],"relativePath":"en-US/resource/index.md","lastUpdated":1650438502000}',n={},d=Object.assign(n,{setup(s){return(c,_)=>{const e=o("Resource");return r(),t("div",null,[a(e)])}}});export{p as __pageData,d as default};
