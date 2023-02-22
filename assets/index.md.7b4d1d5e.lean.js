aaec4c6d113f90d641a6444f991b8bcc62db07ac
ElementPlusBot <hello@element-plus.org>
2023-02-22 11:29:12 +0000

    Deploying to main from @ element-plus/element-plus@f9d4c8c0834cefbac6b85a495ac95140febafd30 🚀

---
+++ assets/index.md.7b4d1d5e.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as t}from"./app.737fc803.js";const _='{"title":"A Vue 3 UI Framework","description":"","frontmatter":{"title":"A Vue 3 UI Framework","lang":"en-US","page":true},"headers":[],"relativePath":"index.md","lastUpdated":1677064912000}',a={},c=Object.assign(a,{__name:"index",setup(r){return(n,o)=>(e(),t("div"))}});export{_ as __pageData,c as default};
