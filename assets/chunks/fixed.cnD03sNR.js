c19404c1ff5a5b63c9ac39c98561e5cf8dac5ea1
ElementPlusBot <element-plus@outlook.com>
2026-01-30 08:55:02 +0000

    Deploying to main from @ element-plus/element-plus@c7e64279b6552924401041e434809b2d586d46f3 🚀

--- assets/chunks/fixed.cnD03sNR.js
+++
@@ -1,1 +0,0 @@
-import{ab as n,ah as e,K as s,v as c,P as t,O as f,Y as r}from"./framework.CgmxGoqY.js";const _={};function p(i,x){const o=e("el-button"),a=e("el-affix");return c(),s(a,{position:"bottom",offset:20},{default:t(()=>[f(o,{type:"primary"},{default:t(()=>[r("Offset bottom 20px")]),_:1})]),_:1})}const m=n(_,[["render",p]]);export{m as E};
